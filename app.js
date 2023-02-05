36519d94c800bae93dc13703b1edcd4bf1e6f3c4
Naimur Rahman <naimurrah01@tamu.edu>
2023-02-05 04:45:11 -0600

    changed port

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 require('dotenv').config()
 
 const app = express();
-const PORT_NUM = process.env.APPSETTING_SERVER_PORT || 9823;
+const PORT_NUM = process.env.APPSETTING_SERVER_PORT || 8000;
 
 app.use(express.json());
 
 app.use(cors());
 
 app.listen(PORT_NUM,  async () => {
-    console.log("This works");
+    console.log("This works at port " + PORT_NUM);
     console.log(process.env);
 });
 
