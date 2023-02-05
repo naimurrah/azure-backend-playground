aaab62f4414ea1ef7fc3a93d2093df9916bdc4ee
Naimur Rahman <naimurrah01@tamu.edu>
2023-02-05 02:59:35 -0600

    changed nodemon to node

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require('dotenv').config()
 
 const app = express();
-const PORT_NUM = process.env.APPSETTING_SERVER_PORT || 8080;
+const PORT_NUM = process.env.APPSETTING_SERVER_PORT || 9823;
 
 app.use(express.json());
 
